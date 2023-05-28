4e2ab0322cdede6e1efba4cba9c935db58af920e
Vann Goh <125730430+vanngoh@users.noreply.github.com>
2023-05-29 03:10:28 +0900

    Fixed the 1 special case

--- main.py
+++ main.py
@@ -1,71 +1,127 @@
+
 import time
 import random
 def solution(buckets):
     n = len(buckets)
-    print("length:", n)
-    index_of_bucket = 1000000
-    num_of_balls = 1000000
+    BITS = 20
+    BUCKET_CONSTRAINT = 1000000
+    BALLS_CONSTRAINT = 1000000
+    smallest_buckets_index = 0
+    balls_to_remove = 0
 
     if n == 1:
         return (0, buckets[0] - 1)
 
-    if n > index_of_bucket:
-        return (0, 0)
-
-    for i in range(n):
-        if buckets[i] > 1000000:
+    if sum(buckets) == n:
+        if n % 2 == 0:
+            return (0, 1)
+        else:
             return (0, 0)
 
-        for balls_to_remove in range(1, buckets[i] + 1):
-            tmp_buckets = buckets[:]
-            tmp_buckets[i] -= balls_to_remove
+    special_index = has_non_one_and_all_ones(buckets)
+    if special_index != False:
+        if n % 2 == 0:
+            return (special_index, buckets[special_index])
+        else:
+            return (special_index, buckets[special_index] - 1)
 
-            # Continue if the current move doesn't have zero NIM sum
-            if check_nim_sum(tmp_buckets, n) is False:
-                continue
-
-            # Current move is a winning move
-            if i < index_of_bucket or (i == index_of_bucket and balls_to_remove < num_of_balls):
-                index_of_bucket = i
-                num_of_balls = balls_to_remove
-                return (index_of_bucket, num_of_balls)
-
-    # No solution for this combination
-    if index_of_bucket == 1000000 or num_of_balls == 1000000:
+    if n > BUCKET_CONSTRAINT or max(buckets) > BALLS_CONSTRAINT:
         return (0, 0)
+    
+    bin_bucket = []
+    nim_sum = [0] * BITS
+    ball_status = [0] * BITS
 
-
-def check_nim_sum(x, n):
-    res = 0
     for i in range(n):
-        res = res ^ x[i]
+        bin_bucket.append(number_to_binary_array(buckets[i], BITS))
+        nim_sum = xor_arrays(nim_sum, bin_bucket[i])
+       
+    print("Bucket", bin_bucket)
+    print("NIM_SUM", nim_sum)
+    
+    largest_bits = find_first_one(nim_sum)
+    
+    # Zero NIM Sum
+    if largest_bits == -1:
+        return(0,0)
 
-    return True if res == 0 else False
+    for k in range(n):
+        if bin_bucket[k][largest_bits] == 1:
+            smallest_buckets_index=k
+            break
+    
+    print("Choosen bucket", smallest_buckets_index)
+    
+    ball_status = xor_arrays(nim_sum, bin_bucket[smallest_buckets_index])
+    balls_to_remove = binary_array_to_number(bin_bucket[smallest_buckets_index]) - binary_array_to_number(ball_status)
+    
+    return (smallest_buckets_index, balls_to_remove)
 
+def has_non_one_and_all_ones(arr):
+    non_one_found = -1
+    for i, num in enumerate(arr):
+        if num != 1:
+            # Found more than one non-one element
+            if non_one_found != -1:
+                return False 
+            non_one_found = i
 
+    return non_one_found
+
+    
+def number_to_binary_array(number, bits=20):
+    bin_str = f"{number:b}"
+    bin_list = [int(bit) for bit in bin_str]  # Convert binary string to list of integers
+
+    if len(bin_list) < bits:
+        bin_list = [0] * (bits - len(bin_list)) + bin_list
+
+    return bin_list
+
+def xor_arrays(arr1, arr2):
+    result = []
+    for i in range(len(arr1)):
+        result.append(arr1[i] ^ arr2[i])
+    return result    
+    
+def find_first_one(array):
+    try:
+        first_position = array.index(1)
+        return first_position
+    except ValueError:
+        return -1    
+
+def binary_array_to_number(binary_array):
+    binary_string = ''.join(map(str, binary_array))
+    number = int(binary_string, 2)
+    return number        
+
+    
 if __name__ == "__main__":
-    assert solution([2, 3]) == (1, 1)
-    assert solution([2, 2]) == (0, 0)
-    assert solution([3, 4, 5, 6]) == (1, 4)
-    assert solution([3, 3]) == (0, 0)
-    assert solution([4, 3]) == (0, 1)
-    assert solution([3, 4]) == (1, 1)
-    assert solution([5, 5]) == (0, 0)
-    assert solution([1, 3, 5, 7]) == (0, 0)
-    assert solution([10]) == (0, 9)
-    assert solution([524286,1,2,8,524287,32]) == (0, 42)
+    # assert solution([2, 3]) == (1, 1)
+    # assert solution([2, 2]) == (0, 0)
+    # assert solution([3, 4, 5, 6]) == (1, 4)
+    # assert solution([3, 3]) == (0, 0)
+    # assert solution([4, 3]) == (0, 1)
+    # assert solution([3, 4]) == (1, 1)
+    # assert solution([5, 5]) == (0, 0)
+    # assert solution([1, 3, 5, 7]) == (0, 0)
+    # assert solution([10]) == (0, 9)
+    # assert solution([524286,1,2,8,524287,32]) == (0, 42)
 
-    # print(solution([40123, 10298, 40123, 990901, 990901, 990901,990901]))
-    # print(solution([1, 802741, 8, 1, 9764, 990901]))
+    # # print(solution([40123, 10298, 40123, 990901, 990901, 990901,990901]))
+    # # print(solution([1, 802741, 8, 1, 9764, 990901]))
     # print(solution([524286,1,2,8,524287,32]))
-    # print(solution([262142,1,2,8,524287,32]))
+    print(solution([1, 3]))
+    # print(solution([7, 1]))
+    # # print(solution([262142,1,2,8,524287,32]))
 
-    arr = []
-    for i in range(10000):
-        arr.append(i)
-    print("Array generated!")
-    t = time.time()
-    print(solution(arr))
-    print("Elapsed:", time.time() - t)
+    # arr = []
+    # for i in range(1, 1000000):
+    #     arr.append(i)
+    # print("Array generated!")
+    # t = time.time()
+    # print(solution(arr))
+    # print("Elapsed:", time.time() - t)
 
     print("All tests passed!")
