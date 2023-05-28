af5b07d88ede2640913ab0638ea0ce3a701b6906
Vann Goh <125730430+vanngoh@users.noreply.github.com>
2023-05-28 19:10:14 +0900

    Enhance & bugs fixed
    - Applied XOR for NIM SUM checking
    - Fixed the wrong test cases

--- main.py
+++ main.py
@@ -1,53 +1,46 @@
-
-def solution(x):
-    n = len(x)
-    index_of_box = 1000000
+import time
+import random
+def solution(buckets):
+    n = len(buckets)
+    print("length:", n)
+    index_of_bucket = 1000000
     num_of_balls = 1000000
 
+    if n == 1:
+        return (0, buckets[0] - 1)
+
+    if n > index_of_bucket:
+        return (0, 0)
+
     for i in range(n):
-        for balls_to_remove in range(1, x[i] + 1):
-            temp_box = x[:]
-            temp_box[i] -= balls_to_remove
+        if buckets[i] > 1000000:
+            return (0, 0)
+
+        for balls_to_remove in range(1, buckets[i] + 1):
+            tmp_buckets = buckets[:]
+            tmp_buckets[i] -= balls_to_remove
 
             # Continue if the current move doesn't have zero NIM sum
-            if check_nim_sum(temp_box) is False:
+            if check_nim_sum(tmp_buckets, n) is False:
                 continue
 
             # Current move is a winning move
-            if i < index_of_box or (i == index_of_box and balls_to_remove < num_of_balls):
-                index_of_box = i
+            if i < index_of_bucket or (i == index_of_bucket and balls_to_remove < num_of_balls):
+                index_of_bucket = i
                 num_of_balls = balls_to_remove
+                return (index_of_bucket, num_of_balls)
 
     # No solution for this combination
-    if index_of_box == 1000000 or num_of_balls == 1000000:
+    if index_of_bucket == 1000000 or num_of_balls == 1000000:
         return (0, 0)
-    
-    # Return the solution
-    return (index_of_box, num_of_balls)
 
 
-def check_nim_sum(x):
-    remainder_poll = 0
-    multiples_poll = 0
+def check_nim_sum(x, n):
+    res = 0
+    for i in range(n):
+        res = res ^ x[i]
 
-    for balls_remained in x:
-        remainder = balls_remained % 2
-        multiples = int(balls_remained / 2)
-
-        # remainder will be either 0 or 1
-        remainder_poll += remainder
-        # multiples will be the multiples of 2
-        multiples_poll += multiples
-
-    # Check if the NIM Sum zero
-    # If it is, the current player is winning
-    return (remainder_poll % 2 == 0) and (multiples_poll % 2 == 0)
-
-
-# Manual check
-# x = [3, 4, 5, 6]
-# result = solution(x)
-# print(f"Index: {result[0]}, Balls to remove: {result[1]}")
+    return True if res == 0 else False
 
 
 if __name__ == "__main__":
@@ -56,8 +49,23 @@ if __name__ == "__main__":
     assert solution([3, 4, 5, 6]) == (1, 4)
     assert solution([3, 3]) == (0, 0)
     assert solution([4, 3]) == (0, 1)
-    assert solution([3, 4]) == (0, 3)
-    assert solution([5, 5]) == (0, 4)
-    assert solution([1, 3, 5, 7]) == (2, 4)
+    assert solution([3, 4]) == (1, 1)
+    assert solution([5, 5]) == (0, 0)
+    assert solution([1, 3, 5, 7]) == (0, 0)
+    assert solution([10]) == (0, 9)
+    assert solution([524286,1,2,8,524287,32]) == (0, 42)
+
+    # print(solution([40123, 10298, 40123, 990901, 990901, 990901,990901]))
+    # print(solution([1, 802741, 8, 1, 9764, 990901]))
+    # print(solution([524286,1,2,8,524287,32]))
+    # print(solution([262142,1,2,8,524287,32]))
+
+    arr = []
+    for i in range(10000):
+        arr.append(i)
+    print("Array generated!")
+    t = time.time()
+    print(solution(arr))
+    print("Elapsed:", time.time() - t)
 
     print("All tests passed!")
